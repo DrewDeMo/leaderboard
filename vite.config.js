779202c3b88107f9fc86a1d81ec4b4ae3acd8c4d
DrewDeMo <drdemaiolo@gmail.com>
2024-10-23 12:42:02 -0400

    fix.

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
-  server: {
-    historyApiFallback: {
-      disableDotRule: true,
-      rewrites: [
-        { from: /^\/admin.*/, to: '/index.html' },
-        { from: /./, to: '/index.html' }
-      ]
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
     }
-  },
-  preview: {
-    port: 5000
   }
 })
